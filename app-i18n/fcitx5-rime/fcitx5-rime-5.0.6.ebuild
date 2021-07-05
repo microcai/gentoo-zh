microcai/gentoo-zh

commit a5840fd96a88f6942a9d295cbc33b974157af0b2

Clean to code in ebuilds (#1078)
* app-i18n/fcitx5: remove unnecessary custom src_install() and the call of xdg_src_prepare()

* app-i18n/fcitx5-chinese-addons: remove unused eclass and the unnecessary calls of functions

* app-i18n/fcitx5-gtk: remove src_install() and migrate xdg-utils.eclass to xdg.eclass

* app-i18n/fcitx5-qt: remove unnecessary src_prepare() and src_install()

* app-i18n/fcitx5-rime: remove unused gnome2-utils.eclass and add xdg.eclass

app-i18n/fcitx5-rime/fcitx5-rime-5.0.6.ebuild
@@ -3,7 +3,7 @@
 
 EAPI=7
 
-inherit cmake gnome2-utils
+inherit cmake xdg
 
 DESCRIPTION="Rime Support for Fcitx5"
 HOMEPAGE="https://github.com/fcitx/fcitx5-rime"
@@ -28,11 +28,3 @@ RDEPEND="app-i18n/fcitx5
 	!app-i18n/fcitx-rime"
 
 DEPEND="${RDEPEND}"
-
-pkg_postinst() {
-	xdg_icon_cache_update
-}
-
-pkg_postrm() {
-	xdg_icon_cache_update
-}

app-i18n/fcitx5-rime/fcitx5-rime-9999.ebuild
@@ -3,7 +3,7 @@
 
 EAPI=7
 
-inherit cmake gnome2-utils
+inherit cmake xdg
 
 DESCRIPTION="Rime Support for Fcitx5"
 HOMEPAGE="https://github.com/fcitx/fcitx5-rime"
@@ -28,11 +28,3 @@ RDEPEND="app-i18n/fcitx5
 	!app-i18n/fcitx-rime"
 
 DEPEND="${RDEPEND}"
-
-pkg_postinst() {
-	xdg_icon_cache_update
-}
-
-pkg_postrm() {
-	xdg_icon_cache_update
-}

app-i18n/fcitx5-chinese-addons/fcitx5-chinese-addons-5.0.6-r1.ebuild
@@ -3,7 +3,7 @@
 
 EAPI=7
 
-inherit cmake gnome2-utils xdg
+inherit cmake xdg
 
 if [[ "${PV}" == 9999 ]]; then
 	inherit git-r3
@@ -47,7 +47,6 @@ src_prepare() {
 	ln -s "${DISTDIR}/fcitx-data-py_stroke-20121124.tar.gz" modules/pinyinhelper/py_stroke-20121124.tar.gz || die
 	ln -s "${DISTDIR}/fcitx-data-py_table-20121124.tar.gz" modules/pinyinhelper/py_table-20121124.tar.gz || die
 	cmake_src_prepare
-	xdg_environment_reset
 }
 
 src_configure() {
@@ -60,8 +59,4 @@ src_configure() {
 		-DUSE_WEBKIT=no
 	)
 	cmake_src_configure
-}
-
-src_install(){
-	cmake_src_install
 }

app-i18n/fcitx5-chinese-addons/fcitx5-chinese-addons-9999.ebuild
@@ -3,7 +3,7 @@
 
 EAPI=7
 
-inherit cmake gnome2-utils xdg
+inherit cmake xdg
 
 if [[ "${PV}" == 9999 ]]; then
 	inherit git-r3
@@ -47,7 +47,6 @@ src_prepare() {
 	ln -s "${DISTDIR}/fcitx-data-py_stroke-20121124.tar.gz" modules/pinyinhelper/py_stroke-20121124.tar.gz || die
 	ln -s "${DISTDIR}/fcitx-data-py_table-20121124.tar.gz" modules/pinyinhelper/py_table-20121124.tar.gz || die
 	cmake_src_prepare
-	xdg_environment_reset
 }
 
 src_configure() {
@@ -60,8 +59,4 @@ src_configure() {
 		-DUSE_WEBKIT=no
 	)
 	cmake_src_configure
-}
-
-src_install(){
-	cmake_src_install
 }

app-i18n/fcitx5-gtk/fcitx5-gtk-5.0.7.ebuild
@@ -3,7 +3,7 @@
 
 EAPI=7
 
-inherit cmake gnome2-utils xdg-utils
+inherit cmake gnome2-utils xdg
 
 if [[ "${PV}" == 9999 ]]; then
 	inherit git-r3
@@ -47,21 +47,14 @@ src_configure() {
 	cmake_src_configure
 }
 
-src_install(){
-	cmake_src_install
-}
 pkg_postinst() {
-	xdg_icon_cache_update
-	xdg_desktop_database_update
-	xdg_mimeinfo_database_update
+	xdg_pkg_postinst
 	use gtk2 && gnome2_query_immodules_gtk2
 	use gtk3 && gnome2_query_immodules_gtk3
 }
 
 pkg_postrm() {
-	xdg_icon_cache_update
-	xdg_desktop_database_update
-	xdg_mimeinfo_database_update
+	xdg_pkg_postrm
 	use gtk2 && gnome2_query_immodules_gtk2
 	use gtk3 && gnome2_query_immodules_gtk3
 }

app-i18n/fcitx5-gtk/fcitx5-gtk-9999.ebuild
@@ -3,7 +3,7 @@
 
 EAPI=7
 
-inherit cmake gnome2-utils xdg-utils
+inherit cmake gnome2-utils xdg
 
 if [[ "${PV}" == 9999 ]]; then
 	inherit git-r3
@@ -47,21 +47,14 @@ src_configure() {
 	cmake_src_configure
 }
 
-src_install(){
-	cmake_src_install
-}
 pkg_postinst() {
-	xdg_icon_cache_update
-	xdg_desktop_database_update
-	xdg_mimeinfo_database_update
+	xdg_pkg_postinst
 	use gtk2 && gnome2_query_immodules_gtk2
 	use gtk3 && gnome2_query_immodules_gtk3
 }
 
 pkg_postrm() {
-	xdg_icon_cache_update
-	xdg_desktop_database_update
-	xdg_mimeinfo_database_update
+	xdg_pkg_postrm
 	use gtk2 && gnome2_query_immodules_gtk2
 	use gtk3 && gnome2_query_immodules_gtk3
 }

app-i18n/fcitx5-qt/fcitx5-qt-5.0.6.ebuild
@@ -32,10 +32,6 @@ RDEPEND="app-i18n/fcitx5
 DEPEND="${RDEPEND}
 	virtual/pkgconfig"
 
-src_prepare() {
-	cmake_src_prepare
-}
-
 src_configure() {
 	local mycmakeargs=(
 		-DCMAKE_INSTALL_SYSCONFDIR="${EPREFIX}/etc"
@@ -46,8 +42,4 @@ src_configure() {
 		-DBUILD_ONLY_PLUGIN=$(usex only_plugin)
 	)
 	cmake_src_configure
-}
-
-src_install(){
-	cmake_src_install
 }

app-i18n/fcitx5-qt/fcitx5-qt-9999.ebuild
@@ -32,10 +32,6 @@ RDEPEND="app-i18n/fcitx5
 DEPEND="${RDEPEND}
 	virtual/pkgconfig"
 
-src_prepare() {
-	cmake_src_prepare
-}
-
 src_configure() {
 	local mycmakeargs=(
 		-DCMAKE_INSTALL_SYSCONFDIR="${EPREFIX}/etc"
@@ -46,8 +42,4 @@ src_configure() {
 		-DBUILD_ONLY_PLUGIN=$(usex only_plugin)
 	)
 	cmake_src_configure
-}
-
-src_install(){
-	cmake_src_install
 }

commit f55ed2b04e537161f07c5e815a26c7b4246d7b3d

app-i18n/fcitx5-rime: fix a build error
Fix a build error introduced in
a5840fd96a88f6942a9d295cbc33b974157af0b2.

If cmake is inherited before xdg and cmake_src_prepare is not called
explicitly, cmake_src_prepare is not called, leading to the failure
of cmake_src_prepare.

app-i18n/fcitx5-rime/fcitx5-rime-5.0.6.ebuild
@@ -3,7 +3,7 @@
 
 EAPI=7
 
-inherit cmake xdg
+inherit xdg cmake
 
 DESCRIPTION="Rime Support for Fcitx5"
 HOMEPAGE="https://github.com/fcitx/fcitx5-rime"

app-i18n/fcitx5-rime/fcitx5-rime-9999.ebuild
@@ -3,7 +3,7 @@
 
 EAPI=7
 
-inherit cmake xdg
+inherit xdg cmake
 
 DESCRIPTION="Rime Support for Fcitx5"
 HOMEPAGE="https://github.com/fcitx/fcitx5-rime"