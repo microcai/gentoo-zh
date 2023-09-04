5467eb9b3bce264984bc3ccaded4907915b484e9
ston <ston.jia@qq.com>
2023-09-04 18:57:07 +0800

    net-im/tencent-qq: add 3.2.0_p16605, drop 3.2.0_p16449
    Signed-off-by: ston <ston.jia@qq.com>

--- net-im/tencent-qq/files/qq.sh
+++ net-im/tencent-qq/files/qq.sh
@@ -4,6 +4,4 @@ if [ -d ~/.config/QQ/versions ]; then
 	find ~/.config/QQ/versions -name sharp-lib -type d -exec rm -r {} \; 2>/dev/null
 fi
 
-export LD_PRELOAD=/usr/lib/gcc/x86_64-pc-linux-gnu/12/libstdc++.so.6
-
 exec /opt/QQ/qq "$@"
