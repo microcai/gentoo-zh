550c65073067f99641f433f510c7e910d4c8a9be
Your Name <you@example.com>
2024-08-12 20:16:07 +0800

    fix failure of net-im/wemeet to start when the language setting is not "zh" or "en"

--- net-im/wemeet/files/wemeetapp-1.sh
+++ net-im/wemeet/files/wemeetapp-1.sh
@@ -7,6 +7,13 @@ export IBUS_USE_PORTAL=1 # fix ibus
 FONTCONFIG_DIR=$HOME/.config/fontconfig
 unset WAYLAND_DISPLAY
 
+# if "^zh" is not detected in $LANG $LC_ALL $LANGUAGE
+if test "${LANG:0:2}" != "zh" -a "${LC_ALL:0:2}" != "zh" -a "${LANGUAGE:0:2}" != "zh"; then
+	export LC_ALL="en_GB.UTF-8"; # 解决非zh或en的语言设置闪退的问题
+else
+	export LC_ALL="zh_CN.UTF-8"; # 解决非zh或en的语言设置闪退的问题
+fi;
+
 # if pipewire-pulse installed
 if [ -f /usr/bin/pipewire-pulse ]; then
 	export PULSE_LATENCY_MSEC=20 # 解决Pipewire播放声音卡顿的问题
